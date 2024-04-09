a90162a8b80e08c8bb5843d74354235e2ea7b2bf
Anthonin <akadi@artfx.fr>
2024-04-09 12:37:30 +0200

    [Fix] Pause Menu

--- SimpleEngineWithOpenGL-011/Game.cpp
+++ SimpleEngineWithOpenGL-011/Game.cpp
@@ -217,32 +217,30 @@ void Game::update(float dt)
 		{
 			delete deadActor;
 		}
-
-		// Update UI screens
-		for (auto ui : UIStack)
+	}
+	// Update UI screens
+	for (auto ui : UIStack)
+	{
+		if (ui->getState() == UIState::Active)
 		{
-			if (ui->getState() == UIState::Active)
-			{
-				ui->update(dt);
-			}
-		}
-
-		// Delete any UIScreens that are closed
-		auto iter = UIStack.begin();
-		while (iter != UIStack.end())
-		{
-			if ((*iter)->getState() == UIState::Closing)
-			{
-				delete* iter;
-				iter = UIStack.erase(iter);
-			}
-			else
-			{
-				++iter;
-			}
+			ui->update(dt);
 		}
 	}
-	
+
+	// Delete any UIScreens that are closed
+	auto iter = UIStack.begin();
+	while (iter != UIStack.end())
+	{
+		if ((*iter)->getState() == UIState::Closing)
+		{
+			delete* iter;
+			iter = UIStack.erase(iter);
+		}
+		else
+		{
+			++iter;
+		}
+	}
 }
 
 void Game::render()
